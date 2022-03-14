b522e4e383a131349d353832b7d828057d871652
stefano.cardinale <stefano.cardinale@stefanocardinales-MacBook-Pro.local>
2022-03-14 22:19:06 +0100

    fixes

--- app.py
+++ app.py
@@ -30,20 +30,24 @@ first_file = appsbasedir + "/saved/adata/GSE110894_I-BET-762.h5ad"
 # data download
 if not os.path.exists('./data'):
     url1 = 'https://drive.google.com/drive/folders/1AH--DDw7_rDDySYlLEo_VgslIbZhFD_Q?usp=sharing'
-    url2 = 'https://drive.google.com/drive/folders/1kTH2hQVNwlXQeV5hCMLVc4eqhrvvag7Q?usp=sharing'
-    url3 = 'https://drive.google.com/drive/folders/1eDHumsD3Cbjd9mVkmnxjbEjNq3qXyg4l?usp=sharing'
-    url4 = 'https://drive.google.com/drive/folders/1o7MjpiQ08Kc0DgTt5Obhazl2YnsAQ-k8?usp=sharing'
-    url5 = 'https://drive.google.com/drive/folders/1XHqaKJt-xQXRADgwCRLHhoGtApHmFfTK?usp=sharing'
-    url6 = 'https://drive.google.com/drive/folders/1G-LnaDTpJm-jy0IDtcbUb7hU4_7uxDHY?usp=sharing'
-
     gdown.download_folder(url1, output='./data/other', quiet=False, use_cookies=False)
+    subprocess.Popen(['mv data/other/*.* data && rm -r data/other'], shell=True)
+if not os.path.exists('./data/GSE117872'):  
+    url2 = 'https://drive.google.com/drive/folders/1kTH2hQVNwlXQeV5hCMLVc4eqhrvvag7Q?usp=sharing'
     gdown.download_folder(url2, output='./data/GSE117872', quiet=False, use_cookies=False)
+if not os.path.exists('./data/GSE110894'):
+    url3 = 'https://drive.google.com/drive/folders/1eDHumsD3Cbjd9mVkmnxjbEjNq3qXyg4l?usp=sharing'
     gdown.download_folder(url3, output='./data/GSE110894', quiet=False, use_cookies=False)
+if not os.path.exists('./data/GSE112274'):
+    url4 = 'https://drive.google.com/drive/folders/1o7MjpiQ08Kc0DgTt5Obhazl2YnsAQ-k8?usp=sharing'
     gdown.download_folder(url4, output='./data/GSE112274', quiet=False, use_cookies=False)
+if not os.path.exists('./data/GSE149383'):
+    url5 = 'https://drive.google.com/drive/folders/1XHqaKJt-xQXRADgwCRLHhoGtApHmFfTK?usp=sharing'
     gdown.download_folder(url5, output='./data/GSE149383', quiet=False, use_cookies=False)
+if not os.path.exists('./data/GSE140440'):
+    url6 = 'https://drive.google.com/drive/folders/1G-LnaDTpJm-jy0IDtcbUb7hU4_7uxDHY?usp=sharing'
     gdown.download_folder(url6, output='./data/GSE140440', quiet=False, use_cookies=False)
 
-    subprocess.Popen(['mv data/other/*.* data && rm -r data/other'], shell=True)
 
 if not os.path.exists('./saved/adata/GSE110894_I-BET-762.h5ad'):
     gdown.download("https://drive.google.com/file/d/1jZj25AEaeE2kCwwbhqkPgm_Qr4miHztN/view?usp=sharing", output='./saved/adata/GSE110894_I-BET-762.h5ad', quiet=False, use_cookies=False, fuzzy=True)
